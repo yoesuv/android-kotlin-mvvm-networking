e6641b959fe02fb678c421f8075bccb14369a3a6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2018-10-15 10:31:38 +0700

    bump target sdk 28 :arrow_up: :penguin:

--- app/src/main/java/com/yoesuv/networkkotlin2/networks/ServiceFactory.kt
+++ app/src/main/java/com/yoesuv/networkkotlin2/networks/ServiceFactory.kt
@@ -8,7 +8,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ServiceFactory {
-    
+
     companion object Factory{
         fun create(): RestApi{
 
