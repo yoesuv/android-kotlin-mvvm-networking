62bd9b32d4d12e3f141f4ba6e78a9337b12460d1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-11-27 10:22:11 +0700

    :hammer: refactor main activity

--- app/src/main/java/com/yoesuv/networkkotlin2/main/views/MainActivity.kt
+++ app/src/main/java/com/yoesuv/networkkotlin2/main/views/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.yoesuv.networkkotlin2.main.views
 
-import androidx.lifecycle.ViewModelProviders
 import androidx.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yoesuv.networkkotlin2.R
 import com.yoesuv.networkkotlin2.databinding.ActivityMainBinding
-import com.yoesuv.networkkotlin2.main.viewmodels.CustomViewModelProvidersMain
-import com.yoesuv.networkkotlin2.main.viewmodels.MainViewModel
-import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
-    private lateinit var viewModel:MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +19,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupBinding(){
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this, CustomViewModelProvidersMain(application, WeakReference(this))).get(MainViewModel::class.java)
-        binding.main = viewModel
+        binding.buttonOne.setOnClickListener {
+
+        }
+        binding.buttonTwo.setOnClickListener {
+
+        }
     }
 
     private fun setupToolbar(){
