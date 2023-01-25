3d6a3c1c5dc7e931b50214852df98be891089b96
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-01-25 17:11:41 +0700

    :pencil: check flavor

--- app/src/main/java/com/yoesuv/networkkotlin2/networks/ListPlaceRepository.kt
+++ app/src/main/java/com/yoesuv/networkkotlin2/networks/ListPlaceRepository.kt
@@ -5,19 +5,21 @@ import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.requests.tryCancel
 import com.github.kittinunf.fuel.gson.responseObject
+import com.yoesuv.networkkotlin2.BuildConfig
 import com.yoesuv.networkkotlin2.data.EndPoint
 import com.yoesuv.networkkotlin2.menu.listplace.models.ListPlaceModel
 import com.yoesuv.networkkotlin2.utils.IdlingResource
 import com.yoesuv.networkkotlin2.utils.debugPrintStackTrace
+import com.yoesuv.networkkotlin2.utils.forTest
 
 class ListPlaceRepository {
 
     private lateinit var requestListPlace: Request
 
     fun getListPlace(onSuccess:(ListPlaceModel) -> Unit, onError:(FuelError) -> Unit) {
-        IdlingResource.increment()
+        if (forTest()) IdlingResource.increment()
         requestListPlace = Fuel.get(EndPoint.LIST_PLACE).responseObject { _, _, result ->
-            IdlingResource.decrement()
+            if (forTest()) IdlingResource.decrement()
             result.fold({
                 onSuccess(it)
             }, {
