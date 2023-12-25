a40222acca6e91b84aa8fcca7d87f50d8e53ad04
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-12-25 09:59:52 +0700

    :pencil: log http request

--- app/src/main/java/com/yoesuv/networkkotlin2/networks/NetworkService.kt
+++ app/src/main/java/com/yoesuv/networkkotlin2/networks/NetworkService.kt
@@ -1,7 +1,10 @@
 package com.yoesuv.networkkotlin2.networks
 
+import com.yoesuv.networkkotlin2.BuildConfig
 import fuel.FuelBuilder
 import okhttp3.OkHttpClient
+
+import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 object NetworkService {
@@ -9,6 +12,13 @@ object NetworkService {
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
         .writeTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        })
         .build()
     val fuel = FuelBuilder().config(httpClient).build()
 }
