dd9fb7ce34272a31cd6c3e7b097233e9e3b56631
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-12-06 07:18:11 +0700

    :newspaper: add resource wrap

---
+++ app/src/main/java/com/yoesuv/networkkotlin2/networks/Resources.kt
@@ -0,0 +1,12 @@
+package com.yoesuv.networkkotlin2.networks
+
+sealed class Resource<T> {
+    class Loading<T> : Resource<T>()
+
+    data class Success<T>(val data: T) : Resource<T>()
+
+    data class Error<T>(val message: String? = null, val exception: Exception? = null) :
+        Resource<T>() {
+        fun getErrorMessage(): String = message ?: exception?.message ?: ""
+    }
+}
