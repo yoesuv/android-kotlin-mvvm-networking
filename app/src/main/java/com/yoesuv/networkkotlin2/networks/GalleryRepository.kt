c617773edb76cacd61287c74c9d24554b8765f26
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-12-06 09:27:57 +0700

    :hammer: refactor request gallery

--- app/src/main/java/com/yoesuv/networkkotlin2/networks/GalleryRepository.kt
+++ app/src/main/java/com/yoesuv/networkkotlin2/networks/GalleryRepository.kt
@@ -1,63 +1,38 @@
 package com.yoesuv.networkkotlin2.networks
 
-import com.google.gson.Gson
-import com.yoesuv.networkkotlin2.data.AppData
 import com.yoesuv.networkkotlin2.data.EndPoint
 import com.yoesuv.networkkotlin2.menu.gallery.models.GalleryModel
 import com.yoesuv.networkkotlin2.utils.IdlingResource
 import com.yoesuv.networkkotlin2.utils.forTest
-import fuel.Request
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.http.encodedPath
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
 
 class GalleryRepository {
 
-    fun getListGallery(
-        scope: CoroutineScope,
-        onSuccess: (GalleryModel) -> Unit,
-        onError: (Throwable) -> Unit
-    ) {
-        scope.launch {
-            if (forTest()) IdlingResource.increment()
-            val request = Request.Builder().url(AppData.BASE_URL + EndPoint.LIST_GALLERY)
-                .build()
-            try {
-                val response = NetworkService.fuel.get(request)
-                val gson = Gson()
-                val data = gson.fromJson(response.body, GalleryModel::class.java)
-                if (forTest()) {
-                    if (!IdlingResource.idlingresource.isIdleNow) {
-                        IdlingResource.decrement()
-                    }
-                }
-                onSuccess(data)
-            } catch (t: Throwable) {
-                if (forTest()) {
-                    if (!IdlingResource.idlingresource.isIdleNow) {
-                        IdlingResource.decrement()
-                    }
-                }
-                onError(t)
-            }
-
-        }
-    }
-
-    fun getListGallery2() = flow {
+    fun getListGallery() = flow<Resource<GalleryModel>> {
         try {
+            if (forTest()) IdlingResource.increment()
+            emit(Resource.Loading())
             val result = client.get {
                 url {
                     encodedPath = EndPoint.LIST_GALLERY
                 }
             }
-            emit(result.body())
+            if (forTest()) {
+                if (!IdlingResource.idlingresource.isIdleNow) {
+                    IdlingResource.decrement()
+                }
+            }
+            emit(Resource.Success(result.body()))
         } catch (e: Exception) {
-            emit(GalleryModel())
-            e.printStackTrace()
+            if (forTest()) {
+                if (!IdlingResource.idlingresource.isIdleNow) {
+                    IdlingResource.decrement()
+                }
+            }
+            emit(Resource.Error("Failed get galleries", e))
         }
     }
 
