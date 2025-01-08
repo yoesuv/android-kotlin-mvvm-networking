316d8ef06e855db04d2f91e509fc04e2a92e8c92
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-01-09 04:59:16 +0700

    :hammer: rename unit test class

--- app/src/test/java/com/yoesuv/networkkotlin2/ListPlaceUnitTest.kt
+++ app/src/test/java/com/yoesuv/networkkotlin2/ListPlaceUnitTest.kt
@@ -4,17 +4,21 @@ import com.yoesuv.networkkotlin2.menu.listplace.models.ListPlaceModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.FixMethodOrder
+import org.junit.runners.MethodSorters
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class ListPlaceUnitTest {
+
     @Test
-    fun addition_isCorrect() {
+    fun response_isCorrect() {
         val response = JsonParser.stringToObject("list_place.json", ListPlaceModel::class.java)
-        println("Response # ${response.data?.size}")
         assertEquals(3, response.data?.size)
     }
+
 }
