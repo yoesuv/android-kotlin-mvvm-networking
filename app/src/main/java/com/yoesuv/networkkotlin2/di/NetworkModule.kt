f79ebb4d833f5278731d594a0ce6f5402523ec61
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-02-05 16:47:29 +0700

    :pencil: module mock

--- app/src/main/java/com/yoesuv/networkkotlin2/di/NetworkModule.kt
+++ app/src/main/java/com/yoesuv/networkkotlin2/di/NetworkModule.kt
@@ -2,12 +2,15 @@ package com.yoesuv.networkkotlin2.di
 
 import com.yoesuv.networkkotlin2.networks.GalleryRepository
 import com.yoesuv.networkkotlin2.networks.GalleryRepositoryImpl
+import com.yoesuv.networkkotlin2.networks.GalleryRepositoryMock
 import com.yoesuv.networkkotlin2.networks.ListPlaceRepository
 import com.yoesuv.networkkotlin2.networks.ListPlaceRepositoryImpl
+import com.yoesuv.networkkotlin2.networks.ListPlaceRepositoryMock
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 import javax.inject.Singleton
 
 @Module
@@ -24,6 +27,24 @@ object NetworkModule {
     @Singleton
     fun provideGalleryRepository() : GalleryRepository {
         return GalleryRepositoryImpl()
+    }
+
+}
+
+@Module
+@TestInstallIn(components = [SingletonComponent::class], replaces = [NetworkModule::class])
+object NetworkModuleMock {
+
+    @Provides
+    @Singleton
+    fun provideListPlaceRepository(): ListPlaceRepository {
+        return ListPlaceRepositoryMock()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGalleryRepository() : GalleryRepository {
+        return GalleryRepositoryMock()
     }
 
 }
